fbeeb01ad73b7c3f5064a5a57a1b65ea7f735775
fsk <fsk@rrtimes.com>
2017-08-08 20:00:19 +0800

    pages

--- mock.js
+++
@@ -1,7 +0,0 @@
-
-import './apps/portal/apps/mk-app-portal-about/mock.js';
-import './apps/portal/apps/mk-app-portal-app1/mock.js';
-import './apps/portal/apps/mk-app-portal-app2/mock.js';
-import './apps/portal/mock.js';
-import './apps/root/apps/mk-app-root-about/mock.js';
-import './apps/root/apps/mk-app-root-helloWorld/mock.js';
