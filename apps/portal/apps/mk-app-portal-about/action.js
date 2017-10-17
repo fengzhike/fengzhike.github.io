98c277eb994f92772f19340a295e06888db98d91
fsk@rrtimes.com <fsk@rrtimes.com>
2017-10-17 16:30:45 +0800

    mk-js

--- apps/portal/apps/mk-app-portal-about/action.js
+++ apps/portal/apps/mk-app-portal-about/action.js
@@ -12,7 +12,7 @@ class action {
     getContent = ()=>{
         return (
             <div>
-            <header style={{'marginBottom':'10px'}}>
+            <header style={{'marginBottom':'20px'}}>
                 <h2 style={{'textAlign':'center','margin':'10px 0'}}>简言</h2>
                 <p>转眼间，从事开发也有几年了。在学习和开发中，时常会有偶得的喜悦，却没有及时记录下来，留下了不少惋惜。开始这篇日志算是亡羊补牢吧，愿从今记录下所经历的点滴</p>
                 <p style={{'margin':'10px 0'}}>苏轼的定风波镇楼，这是我最喜欢的一首词</p>
@@ -27,6 +27,29 @@ class action {
                 料峭春风吹酒醒，微冷，山头斜照却相迎。<br/>回首向来潇瑟处，归去，也无风雨也无晴。
                 </p>
             </section>
+            <hr/>
+            <section>
+                <h3>关于本博客</h3>
+                <p>本博客技术栈：react, redux, immutable,antd,nodejs,hapi,sequelize, node-zookeeper-dubbo</p>
+                <p>本博客基于<a href = "">mk-js</a>框架</p>
+                <div>
+                    <h3>简介</h3>
+                    <p>
+                        mk = Monkey King = 齐天大圣 <br/>
+                        mk系列是一套完整的前、后台解决方案 <br/>
+                        <strong>欢迎satr</strong>
+                    </p>
+                    <h3>特点</h3>
+                    <p>
+                    将网站分成多个独立app，每个app开发模式完全一致，并且可以克隆npmjs发布模板app<br/>
+                    将后台服务分成多个独立service, 每个servie开发模式完全一致， 并且可以克隆npmjs发布的模板service<br/>
+                    开发者自己做的app，service可以发布到npmjs开源给其他开发者使用，成为一个生态化的框架
+                    </p>
+
+                </div>
+
+            </section>
+
         </div>
         )
     }
